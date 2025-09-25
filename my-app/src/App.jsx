9a6cb6746480424d29302f4992d4bb5ffc5b85ea
Dragon Dominik <dragondominik0103@gmail.com>
2025-09-25 12:52:56 +0200

    banner-1

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import bg from "./img/bg.png";
+import { useState, useRef, useEffect } from "react";
+import bg from "/img/bg.png";
 import "./App.css";
 
 import hu from "./locales/hu.json";
@@ -12,10 +12,27 @@ export default function App() {
 
   const browserLang = navigator.language.slice(0, 2).toUpperCase();
   const defaultLang = ["HU", "EN", "SK"].includes(browserLang) ? browserLang : "EN";
-  const [lang, setLang] = useState(defaultLang); //böngésző nyelv
+  const [lang, setLang] = useState(defaultLang);
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  // refs for the language dropdown wrappers (desktop and mobile)
+  const desktopLangRef = useRef(null);
+  const mobileLangRef = useRef(null);
+
+  // close dropdown when clicking outside of either lang wrapper
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (!open) return;
+      const target = e.target;
+      if (desktopLangRef.current && desktopLangRef.current.contains(target)) return;
+      if (mobileLangRef.current && mobileLangRef.current.contains(target)) return;
+      setOpen(false);
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [open]);
 
   return (
     <div id="page" key={lang}>
@@ -23,8 +40,10 @@ export default function App() {
         className="w-screen h-screen bg-cover bg-center"
         style={{ backgroundImage: `url(${bg})` }}
       >
+        <div className="absolute right-0 top-0 w-1/2 lg:w-9/20 h-screen bg-white/55 rounded-l-full custom-ease-in"></div>
+
         {/* Navigation Desktop */}
-        <nav className="w-full justify-between items-center px-2 xl:px-8 py-4 bg-transparent float-from-above hidden lg:flex sintony">
+        <nav className="z-50 w-full justify-between items-center px-2 xl:px-8 py-4 bg-transparent float-from-above hidden lg:flex sintony">
           {/* Left Menu */}
           <div className="flex gap-2 xl:gap-5 text-[var(--light-blue)] shadow-custom">
             <a href="#" className="px-2 xl:px-4 py-2 rounded-[25px] transition-all duration-500 ease-in-out hover:[box-shadow:inset_0_4px_5px_5px_rgb(0,0,0,0.25)]">
@@ -44,10 +63,9 @@ export default function App() {
             </a>
           </div>
 
-
-          {/* Jobb oldal */}
+          {/* Right side */}
           <div className="flex items-center gap-4">
-            {/* Kapcsolat Button */}
+            {/* Contact Button */}
             <button className="bg-[var(--dark-blue)] text-[var(--light-blue)] 
                    px-4 py-2 rounded-[16px] shadow-custom 
                    transition-all duration-500 ease-in-out 
@@ -55,9 +73,8 @@ export default function App() {
               <p className="shadow-custom">{translations[lang].menu.contact}</p>
             </button>
 
-
-            {/* Nyelv lenyíló menü */}
-            <div className="relative">
+            {/* Language dropdown - desktop */}
+            <div className="relative" ref={desktopLangRef}>
               <button
                 onClick={() => setOpen(!open)}
                 className="bg-[var(--dark-blue)] text-[var(--light-blue)] px-2 py-2 rounded-[16px]  shadow-custom flex items-center justify-center gap-1 min-w-[80px] group
@@ -66,7 +83,7 @@ export default function App() {
               >
                 <span className="">{lang}</span>
                 <svg
-                  className={`w-5 h-5 transition-transform duration-200 ${open ? 'rotate-180' : ''}`}
+                  className={`w-4 h-4 transition-transform duration-200 ${open ? "rotate-180" : ""}`}
                   fill="none"
                   stroke="currentColor"
                   viewBox="0 0 24 24"
@@ -76,12 +93,13 @@ export default function App() {
               </button>
 
               {open && (
-                <div className="absolute right-0 mt-3 w-auto bg-[var(--light-blue)] rounded-2xl shadow-xl border border-[var(--dark-blue)] overflow-hidden animate-in fade-in zoom-in-95 duration-200">
-                  {["HU", "EN", "SK"].map((option, index) => (
+                <div className="absolute right-0 mt-2 w-auto bg-[var(--light-blue)]  rounded-[16px] custom-shadow border border-[var(--dark-blue)] overflow-hidden animate-in fade-in zoom-in-95 duration-200">
+                  {["HU", "EN", "SK"].map((option) => (
                     <div
                       key={option}
-                      className={`px-4 py-3 cursor-pointer hover:bg-[var(--dark-blue)] hover:text-[var(--light-blue)] transition-all duration-150 flex items-center justify-center font-medium text-gray-700 hover:text-[var(--dark-blue)] ${option === lang ? 'bg-[var(--darkb-blue)] text-[var(--light-blue)]' : ''
-                        } ${index === 0 ? 'rounded-t-2xl' : ''} ${index === 2 ? 'rounded-b-2xl' : ''}`}
+                      className={`px-4 py-1 cursor-pointer flex items-center justify-center font-medium text-gray-700 
+  transition-colors duration-300 ease-in-out 
+  hover:bg-[var(--dark-blue)] hover:text-[var(--light-blue)]`}
                       onClick={() => {
                         setLang(option);
                         setOpen(false);
@@ -97,7 +115,7 @@ export default function App() {
         </nav>
 
         {/* Navbar Mobile */}
-        <nav className="fixed top-0 left-0 w-full px-4 py-4 z-50 flex lg:hidden items-center justify-between float-from-above sintony">
+        <nav className="relative z-50 top-0 left-0 w-full px-4 py-4 flex lg:hidden items-center justify-between float-from-above sintony">
           {/* Left: MAXCUP */}
           <div className="text-[var(--light-blue)] shadow-custom py-2  font-semibold">
             MAXCUP
@@ -105,8 +123,8 @@ export default function App() {
 
           {/* Right: Menu toggle */}
           <div className="flex items-center h-full">
-            {/* Language Button */}
-            <div className="relative">
+            {/* Language Button - mobile */}
+            <div className="relative" ref={mobileLangRef}>
               <button
                 onClick={() => {
                   setIsMenuOpen(false);
@@ -118,12 +136,13 @@ export default function App() {
               </button>
 
               {open && (
-                <div className="absolute right-0 mt-3 w-auto bg-[var(--light-blue)] rounded-2xl shadow-xl border border-[var(--dark-blue)] overflow-hidden animate-in fade-in zoom-in-95 duration-200">
-                  {["HU", "EN", "SK"].map((option, index) => (
+                <div className="absolute right-0 mt-2 w-auto bg-[var(--light-blue)]  rounded-[16px] custom-shadow border border-[var(--dark-blue)] overflow-hidden animate-in fade-in zoom-in-95 duration-200">
+                  {["HU", "EN", "SK"].map((option) => (
                     <div
                       key={option}
-                      className={`px-4 py-3 cursor-pointer hover:bg-[var(--dark-blue)] hover:text-[var(--light-blue)] transition-all duration-150 flex items-center justify-center font-medium text-gray-700
-          } ${index === 0 ? 'rounded-t-2xl' : ''} ${index === 2 ? 'rounded-b-2xl' : ''}`}
+                      className={`px-4 py-1 cursor-pointer flex items-center justify-center font-medium text-gray-700 
+  transition-colors duration-300 ease-in-out 
+  hover:bg-[var(--dark-blue)] hover:text-[var(--light-blue)]`}
                       onClick={() => {
                         setLang(option);
                         setOpen(false);
@@ -146,7 +165,7 @@ export default function App() {
               aria-label="Toggle menu"
             >
               <svg
-                className={`w-6 h-6 text-[var(--light-blue)] transition-transform duration-300 ${isMenuOpen ? 'rotate-45' : ''}`}
+                className={`w-6 h-6 text-[var(--light-blue)] transition-transform duration-300 ${isMenuOpen ? "rotate-45" : ""}`}
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -164,14 +183,12 @@ export default function App() {
         {/* Mobile Fullscreen Menu */}
         {isMenuOpen && (
           <div
-            className={`fixed inset-0 z-40 flex items-center justify-center transition-opacity duration-300 ${isMenuOpen ? 'opacity-100' : 'opacity-0'
-              }`}
-            onClick={() => setIsMenuOpen(false)} // close when clicking overlay
+            className={`absolute inset-0 z-40 flex items-center justify-center transition-opacity duration-300 ${isMenuOpen ? "opacity-100" : "opacity-0"}`}
+            onClick={() => setIsMenuOpen(false)}
           >
             <div
-              className={`bg-[var(--dark-blue)] w-full h-full flex flex-col items-center justify-center space-y-6 overflow-y-auto transition-transform duration-300 ${isMenuOpen ? 'animate-in fade-in slide-in-from-top duration-300' : 'animate-out fade-out slide-out-to-top duration-300'
-                }`}
-              onClick={(e) => e.stopPropagation()} // prevent closing when clicking menu itself
+              className={`bg-[var(--dark-blue)] w-full h-full flex flex-col items-center justify-center space-y-6 overflow-y-auto transition-transform duration-300 ${isMenuOpen ? "animate-in fade-in slide-in-from-top duration-300" : "animate-out fade-out slide-out-to-top duration-300"}`}
+              onClick={(e) => e.stopPropagation()}
             >
               {[translations[lang].menu.cups, translations[lang].menu.production, translations[lang].menu.renting, translations[lang].menu.washing, translations[lang].menu.logistics, translations[lang].menu.contact].map(
                 (item) => (
@@ -189,9 +206,40 @@ export default function App() {
           </div>
         )}
 
+        <div className="grid grid-cols-[50%_50%] mx-4 xl:mx-8 h-[80vh]">
+          <div className="flex flex-col items-left justify-center h-full custom-ease-in">
+            <div className="hidden lg:block sintony xl-text text-[var(--light-blue)] shadow-custom font-bold -mx-1.5 -my-2 lg:-my-6">
+              MAXCUP
+            </div>
+            <div className="text-[var(--light-blue)] shadow-custom xl-text-between-md-lg">
+              {translations[lang].banner.clean}
+            </div>
 
+            <a
+              href="#contact"
+              className="
+    relative inline-block text-[var(--dark-blue)] bg-[var(--light-blue)] 
+    px-8 py-2 my-2 w-fit rounded-[16px] transition-all duration-500
+    after:content-['»'] after:absolute after:top-1/2 after:right-0 hover:pl-6 hover:pr-10 after:-translate-y-1/2
+    after:opacity-0 after:transition-all after:duration-500
+    hover:after:opacity-100 hover:after:right-6
+  "
+            >
+              {translations[lang].page.offer}
+            </a>
+          </div>
 
+          <div className="flex justify-end items-center h-full pointer-events-none">
+          <img 
+  src="/img/cup1.png" 
+  alt="Cup" 
+  className="h-[70vh] xl:h-[80vh] w-auto max-w-full slide-from-right object-contain" 
+/>
+
+          </div>
+        </div>
       </div>
+      yaho
     </div>
   );
 }
