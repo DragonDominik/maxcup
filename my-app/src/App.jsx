4f408998719d9f34c8bc06128d7f374e6fc576e6
Dragon Dominik <dragondominik0103@gmail.com>
2025-09-25 16:36:02 +0200

    WhyMaxcup?

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -37,7 +37,7 @@ export default function App() {
   return (
     <div id="page" className="absolute top-0 left-0 w-full h-[400vh] bg-[var(--light-blue)]" key={lang}>
       <div
-     className="relative w-screen h-screen bg-cover bg-center m-0 p-0 max-h-[800px] sm:max-h-none"
+        className="relative w-screen h-screen bg-cover bg-center m-0 p-0 max-h-[800px] sm:max-h-none"
         style={{ backgroundImage: `url(${bg})` }}
       >
 
@@ -48,7 +48,7 @@ export default function App() {
         {/* Navigation Desktop */}
         <nav className="relative z-50 top-0 left-0 w-full px-2 py-4 hidden lg:flex items-center justify-between float-from-above sintony">
           {/* Left Menu */}
-          <div className="flex gap-2 xl:gap-5 text-[var(--light-blue)] shadow-custom">
+          <div className="flex gap-2 xl:gap-5 text-[var(--light-blue)] shadow-custom-text">
             <a href="#" className="px-2 xl:px-4 py-2 rounded-[var(--border-radius-16)] transition-all duration-500 ease-in-out hover:[box-shadow:inset_0_4px_5px_5px_rgb(0,0,0,0.25)]">
               {translations[lang].menu.cups}
             </a>
@@ -67,20 +67,20 @@ export default function App() {
           </div>
 
           {/* Right side */}
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-2">
             {/* Contact Button */}
             <button className="bg-[var(--dark-blue)] text-[var(--light-blue)] 
-                   px-4 py-2 rounded-[var(--border-radius-16)] shadow-custom 
+                   px-4 py-2 rounded-[var(--border-radius-16)] shadow-custom-box
                    transition-all duration-500 ease-in-out 
                    hover:bg-[var(--dark-blue-60)] hover:[box-shadow:inset_0_4px_5px_5px_rgb(0,0,0,0.25)]">
-              <p className="shadow-custom">{translations[lang].menu.contact}</p>
+              <p className="">{translations[lang].menu.contact}</p>
             </button>
 
             {/* Language dropdown - desktop */}
             <div className="relative" ref={desktopLangRef}>
               <button
                 onClick={() => setOpen(!open)}
-                className="bg-[var(--dark-blue)] text-[var(--light-blue)] px-2 py-2 rounded-[var(--border-radius-16)]  shadow-custom flex items-center justify-center gap-1 min-w-[80px] group
+                className="bg-[var(--dark-blue)] text-[var(--light-blue)] px-2 py-2 rounded-[var(--border-radius-16)]  shadow-custom-box flex items-center justify-center gap-1 min-w-[80px] group
                           transition-all duration-500 ease-in-out 
                           hover:bg-[var(--dark-blue-60)] hover:[box-shadow:inset_0_4px_5px_5px_rgb(0,0,0,0.25)]"
               >
@@ -96,7 +96,7 @@ export default function App() {
               </button>
 
               {open && (
-                <div className="absolute right-0 mt-2 w-auto bg-[var(--light-blue)]  rounded-[var(--border-radius-16)] custom-shadow border border-[var(--dark-blue)] overflow-hidden animate-in fade-in zoom-in-95 duration-200">
+                <div className="absolute right-0 mt-2 w-auto bg-[var(--light-blue)]  rounded-[var(--border-radius-16)] border border-[var(--dark-blue)] overflow-hidden animate-in fade-in zoom-in-95 duration-200">
                   {["HU", "EN", "SK"].map((option) => (
                     <div
                       key={option}
@@ -120,7 +120,7 @@ export default function App() {
         {/* Navbar Mobile */}
         <nav className="relative z-50 top-0 left-0 w-full px-4 py-4 flex lg:hidden items-center justify-between float-from-above sintony">
           {/* Left: MAXCUP */}
-          <div className="text-[var(--light-blue)] shadow-custom py-2  font-semibold">
+          <div className="text-[var(--light-blue)] shadow-custom-text py-2 font-semibold">
             MAXCUP
           </div>
 
@@ -139,7 +139,7 @@ export default function App() {
               </button>
 
               {open && (
-                <div className="absolute right-0 mt-2 w-auto bg-[var(--light-blue)]  rounded-[var(--border-radius-16)] custom-shadow border border-[var(--dark-blue)] overflow-hidden animate-in fade-in zoom-in-95 duration-200">
+                <div className="absolute right-0 mt-2 w-auto bg-[var(--light-blue)]  rounded-[var(--border-radius-16)] border border-[var(--dark-blue)] overflow-hidden animate-in fade-in zoom-in-95 duration-200">
                   {["HU", "EN", "SK"].map((option) => (
                     <div
                       key={option}
@@ -211,10 +211,10 @@ export default function App() {
 
         <div className="hidden md:grid grid-cols-[50%_50%] h-[80vh] max-h-[1200px] relative">
           <div className="flex flex-col items-left justify-center h-full custom-ease-in pl-8 xl:pl-12">
-            <div className="hidden lg:block sintony xl-text text-[var(--light-blue)] shadow-custom font-bold -mx-1.5 -my-2 lg:-my-6">
+            <div className="hidden lg:block sintony xl-text text-[var(--light-blue)] shadow-custom-text font-bold -mx-1.5 -my-2 lg:-my-6">
               MAXCUP
             </div>
-            <div className="text-[var(--light-blue)] shadow-custom xl-text-between-md-lg">
+            <div className="text-[var(--light-blue)] shadow-custom-text xl-text-between-md-lg">
               {translations[lang].banner.clean}
             </div>
 
@@ -225,7 +225,7 @@ export default function App() {
                 px-8 py-2 my-2 w-fit rounded-[var(--border-radius-16)] transition-all duration-500
                 after:content-['»'] after:absolute after:top-1/2 after:right-0 hover:pl-6 hover:pr-10 after:-translate-y-1/2
                 after:opacity-0 after:transition-all after:duration-500
-                hover:after:opacity-100 hover:after:right-6
+                hover:after:opacity-100 hover:after:right-6 shadow-custom-box
               "
             >
               {translations[lang].page.offer}
@@ -240,31 +240,59 @@ export default function App() {
           </div>
         </div>
 
-<div className="md:hidden absolute left-0 right-0 bottom-0 z-30 w-full h-[80vh] overflow-hidden flex flex-col justify-end items-center">
+        <div className="md:hidden absolute left-0 right-0 bottom-0 z-30 w-full h-[80vh] overflow-hidden flex flex-col justify-end items-center">
 
- {/* Background image */}
-  <div
-    className="absolute bottom-[-20%] right-[-20%] w-[120%] h-[120%] bg-no-repeat bg-contain bg-right-bottom pointer-events-none slide-from-right"
-    style={{ backgroundImage: "url('/img/cup1.png')" }}
-  />
-  
-  {/* Button */}
-  <a
-    href="#contact"
-    className="
+          {/* Background image */}
+          <div
+            className="absolute bottom-[-20%] right-[-20%] w-[120%] h-[120%] bg-no-repeat bg-contain bg-right-bottom pointer-events-none slide-from-right"
+            style={{ backgroundImage: "url('/img/cup1.png')" }}
+          />
+
+          {/* Button */}
+          <a
+            href="#contact"
+            className="
       relative inline-block text-[var(--dark-blue)] bg-[var(--light-blue)] 
       px-8 py-2 mb-10 w-fit rounded-[var(--border-radius-16)] transition-all duration-500
       after:content-['»'] after:absolute after:top-1/2 after:right-0 hover:pl-6 hover:pr-10 after:-translate-y-1/2
       after:opacity-0 after:transition-all after:duration-500
-      hover:after:opacity-100 hover:after:right-6 custom-ease-in-late
+      hover:after:opacity-100 hover:after:right-6 custom-ease-in-late shadow-box
     "
-  >
-    {translations[lang].page.offer}
-  </a>
-</div>
+          >
+            {translations[lang].page.offer}
+          </a>
+        </div>
 
       </div>
-      yaho
+
+
+
+    {/* Miért a maxcup? */}
+  <div className="flex justify-center items-center relative w-screen h-fit m-0 p-0 mt-10 lg:mt-20">
+        <div className="grid grid-cols-1 gap-1 lg:grid-cols-2 w-[90%] lg:w-[85%]">
+          <div className="order-1 lg:order-1">
+            <h1 className="lg-text sintony font-bold">Miért a MaxCup?</h1>
+            <p className="whitespace-pre-wrap text-justify">
+              {translations[lang].whymaxcup.paragraph}
+            </p>
+          </div>
+
+          <div className="order-3 lg:order-2 mt-2 lg:mt-0 flex justify-center items-center">
+            <video src="/img/maxvideo.webm" autoPlay loop muted playsInline className="h-[90vw] w-[90vw] lg:h-[40vw] lg:w-[40vw] object-cover rounded-[var(--border-radius-50)] border-15 lg:border-10 border-[var(--mid-blue)]" ></video>
+          </div>
+
+          <div className="hidden md:flex order-2 lg:order-3 lg:col-span-2">
+            <div className="text-left space-x-1.5 space-y-2 sm-text lg:md-text lg:space-y-0">
+              <div className="inline-block px-3 py-2 rounded-[var(--border-radius-24)] shadow-custom-box bg-[var(--mid-blue)]">{translations[lang].whymaxcup.sales}</div>
+              <div className="inline-block px-3 py-2 rounded-[var(--border-radius-24)] shadow-custom-box bg-[var(--mid-blue)]">{translations[lang].whymaxcup.cupsale}</div>
+               <div className="inline-block px-3 py-2 rounded-[var(--border-radius-24)] shadow-custom-box bg-[var(--mid-blue)]">{translations[lang].whymaxcup.token}</div>
+            </div>
+
+
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
